29332904903a7ca99e67e582b553454ba14a4b2a
Amel TRIKI <amel_triki@yahoo.fr>
2020-09-15 13:57:38 +0200

    TP1 EX1 finalisé

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './MonSite/toolbar/toolbar.component';
+import { ListComponent } from './MonSite/list/list.component';
+import { FooterComponent } from './MonSite/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    ListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
